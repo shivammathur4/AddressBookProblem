9c5f4bf613edf07b7dc06a7607b6b6fd036e1c0c
shivammathur4 <shivamm.4aug@gmail.com>
2021-05-04 10:30:53 +0530

    Added UC1

---
+++ AddressBookProblem/Contact.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    class Contacts
+    {
+        //variables
+        string first_name;
+        string last_name;
+        string address;
+        string city;
+        string state;
+        int zip;
+        long phone;
+        string email;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Contact"/> class.
+        /// </summary>
+        /// <param name="firstName">The first name.</param>
+        /// <param name="lastName">The last name.</param>
+        /// <param name="address">The address.</param>
+        /// <param name="city">The city.</param>
+        /// <param name="state">The state.</param>
+        /// <param name="zip">The zip.</param>
+        /// <param name="phoneNumber">The phone number.</param>
+        /// <param name="email">The email.</param>
+        public Contacts(string first_name, string last_name, string address, string city, string state, int zip, long phone, string email)
+        {
+            this.first_name = first_name;
+            this.last_name = last_name;
+            this.address = address;
+            this.city = city;
+            this.state = state;
+            this.zip = zip;
+            this.phone = phone;
+            this.email = email;
+        }
+    }
+}
--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -8,9 +8,13 @@ namespace AddressBookProblem
 {
     class Program
     {
+        /// <summary>
+        /// Defines the entry point of the application and also implementation of main class.
+        /// </summary>
+        /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcome to Address Book!");
+            Console.WriteLine("Hello, Welcome to Address Book Program");
         }
     }
 }
