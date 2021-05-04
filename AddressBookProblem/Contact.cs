88e8d5aead4f979d2e27f4cbe7f780cca5ee095f
shivammathur4 <shivamm.4aug@gmail.com>
2021-05-04 10:33:53 +0530

    Added UC2

--- AddressBookProblem/Contact.cs
+++ AddressBookProblem/Contact.cs
@@ -6,28 +6,39 @@ using System.Threading.Tasks;
 
 namespace AddressBookProblem
 {
-    class Contacts
+    public class Contacts
     {
+        //list is created to store contact info
+        List<Contacts> contacts = new List<Contacts>();
+
         //variables
-        string first_name;
-        string last_name;
-        string address;
-        string city;
-        string state;
-        int zip;
-        long phone;
-        string email;
+        public string first_name;
+        public string last_name;
+        public string address;
+        public string city;
+        public string state;
+        public int zip;
+        public long phone;
+        public string email;
 
         /// <summary>
-        /// Initializes a new instance of the <see cref="Contact"/> class.
+        /// Initializes a new instance of the <see cref="Contacts"/> class.
         /// </summary>
-        /// <param name="firstName">The first name.</param>
-        /// <param name="lastName">The last name.</param>
+        public Contacts()
+        {
+
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Contacts"/> class.
+        /// </summary>
+        /// <param name="first_name">The first name.</param>
+        /// <param name="last_name">The last name.</param>
         /// <param name="address">The address.</param>
         /// <param name="city">The city.</param>
         /// <param name="state">The state.</param>
         /// <param name="zip">The zip.</param>
-        /// <param name="phoneNumber">The phone number.</param>
+        /// <param name="phone">The phone.</param>
         /// <param name="email">The email.</param>
         public Contacts(string first_name, string last_name, string address, string city, string state, int zip, long phone, string email)
         {
@@ -40,5 +51,44 @@ namespace AddressBookProblem
             this.phone = phone;
             this.email = email;
         }
+
+        /// <summary>
+        /// Sets the contacts.
+        /// </summary>
+        /// <param name="contacts">The contacts.</param>
+        public void setContacts(List<Contacts> contacts)
+        {
+            this.contacts = contacts;
+        }
+
+        /// <summary>
+        /// Gets the contacts.
+        /// </summary>
+        /// <returns></returns>
+        public List<Contacts> getContacts()
+        {
+            return contacts;
+        }
+
+        /// <summary>
+        /// Shows the contacts.
+        /// </summary>
+        /// <returns></returns>
+        public List<Contacts> showContacts()
+        {
+            return contacts;
+        }
+
+        /// <summary>
+        /// Converts to string.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString()
+        {
+            return "First Name: " + first_name + ", " + "Last Name: " + last_name + ", " + "Address: " + address + ", " + "City: " + city + ", " + "State: " + state + ", " + "Zip: " + zip + ", Phone Number: " + phone + ", Email-id: " + email;
+        }
+
     }
 }
--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -9,12 +9,62 @@ namespace AddressBookProblem
     class Program
     {
         /// <summary>
-        /// Defines the entry point of the application and also implementation of main class.
+        /// Defines the entry point of the application.
         /// </summary>
         /// <param name="args">The arguments.</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, Welcome to Address Book Program");
+            //creating a list
+            List<Contacts> contacts = new List<Contacts>();
+
+            Console.WriteLine("Hello, Welcome to Address Book!");
+            //choice = 0 means choice has not made yet
+            int choice = 0;
+            while (choice != 5)
+            {
+                Console.WriteLine("\t********Main Menu***********\t");
+                Console.WriteLine("Enter the following choice");
+                Console.WriteLine("1. Add Contact");
+                Console.WriteLine("2. Display Contacts");
+                Console.WriteLine("3. Exit");
+                Console.WriteLine("Enter your choice: ");
+                choice = Convert.ToInt32(Console.ReadLine());
+
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine("Add New Contacts: ");
+                        Console.WriteLine("Enter the Firstname: ");
+                        string first_name = Console.ReadLine();
+                        Console.WriteLine("Enter the Lastname: ");
+                        string last_name = Console.ReadLine();
+                        Console.WriteLine("Enter the Address: ");
+                        string address = Console.ReadLine();
+                        Console.WriteLine("Enter the City: ");
+                        string city = Console.ReadLine();
+                        Console.WriteLine("Enter the State: ");
+                        string state = Console.ReadLine();
+                        Console.WriteLine("Enter the Zip: ");
+                        int zip = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Enter the Phone number");
+                        long phone = Convert.ToInt64(Console.ReadLine());
+                        Console.WriteLine("Enter the Email: ");
+                        string email = Console.ReadLine();
+
+                        //Reference for c
+                        Contacts ct1 = new Contacts(first_name, last_name, address, city, state, zip, phone, email);
+                        contacts.Add(ct1);
+                        Console.WriteLine("Contact Added Successfully");
+                        break;
+
+                    case 4:
+                        foreach (Contacts c in contacts)
+                        {
+                            Console.WriteLine(c);
+                        }
+                        break;
+                }
+            }
         }
     }
 }
