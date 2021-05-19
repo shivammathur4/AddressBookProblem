9aa0e4317cc68463677721de8475c3587d15a7e4
shivammathur4 <shivamm.4aug@gmail.com>
2021-05-20 03:19:47 +0530

    Added  new contact to address book class

---
+++ AddressBookProblem/AddressBook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookProblem
+{
+    class AddressBook
+    {
+        
+        public static List<Contact> People;
+
+        
+        public AddressBook()
+        {
+            People = new List<Contact>();
+        }
+
+        
+        public Contact FindContact(string fname)
+        {
+            Contact contact = People.Find((person) => person.FirstName == fname);
+            return contact;
+        }
+
+        
+        public bool AddContact(string FirstName, string LastName, string Address, string City, string State, string ZipCode, string PhoneNumber, string Email)
+        {
+            Contact contact = new Contact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email);
+            Contact result = FindContact(FirstName);
+            if (result == null)
+            {
+                People.Add(contact);
+                return true;
+            }
+            else
+                return false;
+        }
+    }
+}
--- AddressBookProblem/Program.cs
+++ AddressBookProblem/Program.cs
@@ -8,62 +8,53 @@ namespace AddressBookProblem
 {
     class Program
     {
-        /// <summary>
-        /// Defines the entry point of the application.
-        /// </summary>
-        /// <param name="args">The arguments.</param>
+        
+        AddressBook book;
+
+        
+        public Program()
+        {
+            book = new AddressBook();
+        }
+
+        
         static void Main(string[] args)
         {
-            //creating a list
-            List<Contacts> contacts = new List<Contacts>();
-
+            
+            Program p = new Program();
             Console.WriteLine("Hello, Welcome to Address Book!");
-            //choice = 0 means choice has not made yet
-            int choice = 0;
-            while (choice != 5)
+            Console.WriteLine("Select the option. \n1. Add new contact. \n2. Edit existing contact. \n3. Exit.");
+            int option = int.Parse(Console.ReadLine());
+            switch (option)
             {
-                Console.WriteLine("\t********Main Menu***********\t");
-                Console.WriteLine("Enter the following choice");
-                Console.WriteLine("1. Add Contact");
-                Console.WriteLine("2. Display Contacts");
-                Console.WriteLine("3. Exit");
-                Console.WriteLine("Enter your choice: ");
-                choice = Convert.ToInt32(Console.ReadLine());
-
-                switch (choice)
-                {
-                    case 1:
-                        Console.WriteLine("Add New Contacts: ");
-                        Console.WriteLine("Enter the Firstname: ");
-                        string first_name = Console.ReadLine();
-                        Console.WriteLine("Enter the Lastname: ");
-                        string last_name = Console.ReadLine();
-                        Console.WriteLine("Enter the Address: ");
-                        string address = Console.ReadLine();
-                        Console.WriteLine("Enter the City: ");
-                        string city = Console.ReadLine();
-                        Console.WriteLine("Enter the State: ");
-                        string state = Console.ReadLine();
-                        Console.WriteLine("Enter the Zip: ");
-                        int zip = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter the Phone number");
-                        long phone = Convert.ToInt64(Console.ReadLine());
-                        Console.WriteLine("Enter the Email: ");
-                        string email = Console.ReadLine();
-
-                        //Reference for c
-                        Contacts ct1 = new Contacts(first_name, last_name, address, city, state, zip, phone, email);
-                        contacts.Add(ct1);
-                        Console.WriteLine("Contact Added Successfully");
+                case 1:
+                    Console.WriteLine("Enter the person details to be added in the address book");
+                    Console.WriteLine("First Name");
+                    string FirstName = Console.ReadLine();
+                    Console.WriteLine("Last Name");
+                    string LastName = Console.ReadLine();
+                    Console.WriteLine("Address");
+                    string Address = Console.ReadLine();
+                    Console.WriteLine("City");
+                    string City = Console.ReadLine();
+                    Console.WriteLine("State");
+                    string State = Console.ReadLine();
+                    Console.WriteLine("Zip code");
+                    string ZipCode = Console.ReadLine();
+                    Console.WriteLine("Phone Number");
+                    string PhoneNumber = Console.ReadLine();
+                    Console.WriteLine("Email");
+                    string Email = Console.ReadLine();
+                    if (p.book.AddContact(FirstName, LastName, Address, City, State, ZipCode, PhoneNumber, Email))
+                    {
+                        Console.WriteLine("Contact added successfully");
                         break;
-
-                    case 4:
-                        foreach (Contacts c in contacts)
-                        {
-                            Console.WriteLine(c);
-                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("Contact already exists");
                         break;
-                }
+                    }
             }
         }
     }
